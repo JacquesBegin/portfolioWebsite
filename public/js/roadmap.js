f8b1d6127b1d8af3aa300e71f5b8f2d57d27fe9b
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-14 14:50:21 -0500

    Adds functionality to change level selector color on click.

--- public/js/roadmap.js
+++ public/js/roadmap.js
@@ -1,3 +1,15 @@
 function createLevelSelector() {
-  
+  let rating = document.getElementById("levelSelector");
+  // console.log(rating);
+    for(let node of rating.children) {
+      node.addEventListener("click", function() {
+        console.log(this.style);
+        let box = this.style.backgroundColor = "#00ffbf"
+        
+      })
+    }
 }
+
+
+
+createLevelSelector();
