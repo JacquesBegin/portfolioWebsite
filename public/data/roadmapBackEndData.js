0ea972954ed73536151f07aef81db26174bafd88
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-17 18:08:59 -0500

    Creates back end data file without data.

---
+++ public/data/roadmapBackEndData.js
@@ -0,0 +1,43 @@
+let roadmapBackEndData = {
+  "data": [
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "HTML",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Learn The Basics",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "1c2bede9-8a42-4c07-8e24-4a1706079a3d",
+          "name": "Writing Semantic HTML",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "8cdece69-d8e5-466d-9254-5f63e05322ca",
+          "name": "Basic SEO",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "956c403b-bbc5-4f3c-b5ef-31aba37b2f78",
+          "name": "Accessibility",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    }
+  ],
+  "roadmapTitle": "Back End Web Development Roadmap/Skills"
+}
