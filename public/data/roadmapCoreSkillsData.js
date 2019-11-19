bd2cb067808a19c70a22d9544f0de585698fc988
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-18 22:34:34 -0500

    Adds core skills roadmap data.

--- public/data/roadmapCoreSkillsData.js
+++ public/data/roadmapCoreSkillsData.js
@@ -2,35 +2,167 @@ let roadmapCoreSkillsData = {
   "data": [
     {
       "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "HTML",
+      "sectionTitle": "Git",
       "topics": [
         {
           "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Learn The Basics",
+          "name": "Version Control",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    },
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "Basic Terminal Usage",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Mac",
           "level": 1,
           "subtopics": [],
           "resources": [],
           "examples": []
         },
         {
-          "id": "1c2bede9-8a42-4c07-8e24-4a1706079a3d",
-          "name": "Writing Semantic HTML",
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Linux",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    },
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "SSH",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Secure Shell",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    },
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "Data Structures and Algorithms",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Data Structures",
           "level": 1,
           "subtopics": [],
           "resources": [],
           "examples": []
         },
         {
-          "id": "8cdece69-d8e5-466d-9254-5f63e05322ca",
-          "name": "Basic SEO",
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Algorithms",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    },
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "HTTP/HTTPS and API's",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "HTTP",
           "level": 1,
           "subtopics": [],
           "resources": [],
           "examples": []
         },
         {
-          "id": "956c403b-bbc5-4f3c-b5ef-31aba37b2f78",
-          "name": "Accessibility",
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "HTTPS",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "API's",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    },
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "Design Patterns",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "YAGNI",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "KISS",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "SOLID",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        }
+      ]
+    },
+    {
+      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
+      "sectionTitle": "Additional Topics",
+      "topics": [
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "GitHub",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Licenses",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Semantic Versioning",
+          "level": 1,
+          "subtopics": [],
+          "resources": [],
+          "examples": []
+        },
+        {
+          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
+          "name": "Character Encoding",
           "level": 1,
           "subtopics": [],
           "resources": [],
