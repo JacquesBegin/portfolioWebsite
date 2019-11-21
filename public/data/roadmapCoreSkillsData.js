ab42ddfc23f7a38261c0c967bd1cfecb980675a9
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-20 23:44:16 -0500

    Updates id's to data.

--- public/data/roadmapCoreSkillsData.js
+++ public/data/roadmapCoreSkillsData.js
@@ -1,179 +1,178 @@
 let roadmapCoreSkillsData = {
-  "data": [
+  data: [
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "Git",
-      "topics": [
+      id: "9a747c7c-aa74-4e88-b75a-d0b9a2b2c7d5",
+      sectionTitle: "Git",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Version Control",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "f62611f8-4a77-4190-8f72-731fb42073b0",
+          name: "Version Control",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     },
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "Basic Terminal Usage",
-      "topics": [
+      id: "0ed78862-bfef-448a-9480-233f82102c69",
+      sectionTitle: "Basic Terminal Usage",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Mac",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "a3177e86-68c1-4866-8092-de781cd29d02",
+          name: "Mac",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Linux",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "ea991b32-b4a4-4782-ad77-3f284e326c25",
+          name: "Linux",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     },
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "SSH",
-      "topics": [
+      id: "be69a13a-bf55-44ed-8874-2a109d391d14",
+      sectionTitle: "SSH",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Secure Shell",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "e3b5aaa3-e247-4148-a83d-8dd2b825cbcc",
+          name: "Secure Shell",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     },
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "Data Structures and Algorithms",
-      "topics": [
+      id: "8440f91f-8c67-4be0-ac63-39926b74f948",
+      sectionTitle: "Data Structures and Algorithms",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Data Structures",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "335310fd-6cec-4985-9c1e-f68644b0c085",
+          name: "Data Structures",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Algorithms",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "61009d82-4042-4340-9c6d-1158134c5fc3",
+          name: "Algorithms",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     },
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "HTTP/HTTPS and API's",
-      "topics": [
+      id: "34dc7709-1018-4694-b856-ec2a83f28bb4",
+      sectionTitle: "HTTP/HTTPS and API's",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "HTTP",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "e2f0e3d7-d098-47dc-9521-177afa69e230",
+          name: "HTTP",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "HTTPS",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "f618ad50-fe80-4f61-a223-659da07bf95d",
+          name: "HTTPS",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "API's",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "7dbe836f-47c7-4016-9e13-66bccfde4c33",
+          name: "API's",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     },
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "Design Patterns",
-      "topics": [
+      id: "f58e19b8-0b34-4d0a-9b40-0fe4be0d57d6",
+      sectionTitle: "Design Patterns",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "YAGNI",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "e9cf3402-636d-4608-b445-71feea9e5e92",
+          name: "YAGNI",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "KISS",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "4ffc30fb-a528-400c-b94e-460e423a7205",
+          name: "KISS",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "SOLID",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "aecfd1b2-5fd5-4da5-83a7-9f6f8a47c7f2",
+          name: "SOLID",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     },
     {
-      "id": "29ba1c01-423f-40f0-bc28-e7a842e3eda3",
-      "sectionTitle": "Additional Topics",
-      "topics": [
+      id: "3f3978e5-3123-47f7-87bb-edb4b4919184",
+      sectionTitle: "Additional Topics",
+      topics: [
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "GitHub",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "318bcaf1-6e0f-4525-b7a3-03cefa290c8d",
+          name: "GitHub",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Licenses",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "5f2f8a2f-9f2c-4021-bed3-55eb5987c76c",
+          name: "Licenses",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Semantic Versioning",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "4f9090b8-25db-4038-83d0-c46db5ec7c82",
+          name: "Semantic Versioning",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         },
         {
-          "id": "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
-          "name": "Character Encoding",
-          "level": 1,
-          "subtopics": [],
-          "resources": [],
-          "examples": []
+          id: "dafe6413-d6a8-44c1-b794-5a98ca8f1c47",
+          name: "Character Encoding",
+          level: 1,
+          subtopics: [],
+          resources: [],
+          examples: []
         }
       ]
     }
   ],
-  "roadmapTitle": "Core Skills Web Development Roadmap",
-  "fileName": "roadmapCoreSkillsData"
-}
+  roadmapTitle: "Core Skills Web Development Roadmap",
+  fileName: "roadmapCoreSkillsData"
+};
 
-// Use only for creating new id's
 module.exports = roadmapCoreSkillsData;
