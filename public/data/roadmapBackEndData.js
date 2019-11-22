01d0d1e3ee12b19d42ec6f1bb5ffab08407e5ec7
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-22 17:11:52 -0500

    Updates skill level for back end data.

--- public/data/roadmapBackEndData.js
+++ public/data/roadmapBackEndData.js
@@ -7,12 +7,12 @@ let roadmapBackEndData = {
         {
           id: "1d634752-5f23-4a34-a2d2-5ca76cad13b8",
           name: "NodeJS",
-          level: 1,
+          level: 2,
           subtopics: [
             {
               id: "30b41e41-8aa9-4a4c-8552-5cad9561277c",
               name: "JavaScript",
-              level: 1,
+              level: 3,
               subtopics: [],
               resources: [],
               examples: []
@@ -20,7 +20,7 @@ let roadmapBackEndData = {
             {
               id: "04efaab6-502b-4eea-aa9f-deaaad930a95",
               name: "TypeScript",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -38,12 +38,12 @@ let roadmapBackEndData = {
         {
           id: "6f665ccf-33ba-46df-9013-392a3b01e9f6",
           name: "Fetch And Save Reddit Posts",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "3482ed00-5be9-4485-be75-a1ff12551630",
               name: "Get Posts From /r/programming",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -55,12 +55,12 @@ let roadmapBackEndData = {
         {
           id: "46b9a5cc-affb-4a83-b3a2-f3b4145ca800",
           name: "Create Directory Structure",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "e220b147-55f1-4456-89d4-a1c32fdd5f36",
               name: "In JSON Format",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -72,12 +72,12 @@ let roadmapBackEndData = {
         {
           id: "fd3af559-283d-43b1-940c-46db75b350f2",
           name: "Read JSON Directory Data",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "acd17fba-02b5-4516-adca-145f8768875b",
               name: "Create Directory Structure",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -89,7 +89,7 @@ let roadmapBackEndData = {
         {
           id: "62c4b9bd-ea2d-433d-9628-a5163f39d36a",
           name: "Automate A Common Task",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -103,7 +103,7 @@ let roadmapBackEndData = {
         {
           id: "3fdd8b28-f00e-4141-8efc-ba17244aec94",
           name: "NPM",
-          level: 1,
+          level: 2,
           subtopics: [],
           resources: [],
           examples: []
@@ -117,7 +117,7 @@ let roadmapBackEndData = {
         {
           id: "14bcc7b8-eb8b-49cc-bce7-76c287f819d7",
           name: "NodeJS",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -125,12 +125,12 @@ let roadmapBackEndData = {
         {
           id: "5afcaddf-3e0d-4fe7-b2c4-78f48e62363b",
           name: "Security",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "ead42f60-1e8f-4c6f-a213-9d2c380e4918",
               name: "OWASP",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -148,12 +148,12 @@ let roadmapBackEndData = {
         {
           id: "e6b4848d-7e7a-4b2c-9c2f-696ed22d08ac",
           name: "Create Package",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "e166381f-29e2-4c75-ac7c-2c231b1afd23",
               name: "Use Standards And Best Practices",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -165,7 +165,7 @@ let roadmapBackEndData = {
         {
           id: "981e1f45-2ffe-47e7-90ca-6c9cebc37a9c",
           name: "Distribute Package",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -184,7 +184,7 @@ let roadmapBackEndData = {
             {
               id: "9aa6ed7d-c051-4860-a97f-015efac08ce3",
               name: "Refactor And Implement Best Practices",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -192,7 +192,7 @@ let roadmapBackEndData = {
             {
               id: "ef76dcbe-2441-46ec-bd78-b4732cd3938b",
               name: "Resolve Open Issues",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -200,7 +200,7 @@ let roadmapBackEndData = {
             {
               id: "b70484f6-e738-4d64-a633-876173c12576",
               name: "Add Additional Functionality",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -218,7 +218,7 @@ let roadmapBackEndData = {
         {
           id: "ef26bb2e-bfd1-413d-968c-a24d4532522f",
           name: "Unit And Integration Testing",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -226,7 +226,7 @@ let roadmapBackEndData = {
         {
           id: "8b691eb7-69e5-4239-9f40-d5509930ab0e",
           name: "Learn Testing Terminologies",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -234,12 +234,12 @@ let roadmapBackEndData = {
         {
           id: "ffffa475-a0b9-4f78-8b7e-22c3b5714891",
           name: "Testing Frameworks",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "050259f1-6375-4978-bc79-a58519807f57",
               name: "Mocha",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -247,7 +247,7 @@ let roadmapBackEndData = {
             {
               id: "ccc7b747-11c9-4845-92fc-42fb80f2743f",
               name: "Chai",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -255,7 +255,7 @@ let roadmapBackEndData = {
             {
               id: "c4d51d01-175f-42b8-bdf4-26dc008913df",
               name: "Sinon",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -263,7 +263,7 @@ let roadmapBackEndData = {
             {
               id: "d56e78f3-12fe-4298-8083-b82d39e487bd",
               name: "Mockery",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -271,7 +271,7 @@ let roadmapBackEndData = {
             {
               id: "2ffcdc77-f3ef-4491-ac09-8ae685d30764",
               name: "Ava",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -279,7 +279,7 @@ let roadmapBackEndData = {
             {
               id: "72688b2e-9873-4441-bd5b-995051b728cd",
               name: "Jasmine",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -291,7 +291,7 @@ let roadmapBackEndData = {
         {
           id: "2aa8ef0d-a632-44f6-9e45-3cbcca535223",
           name: "Write Tests For Completed Projects",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -305,7 +305,7 @@ let roadmapBackEndData = {
         {
           id: "2b5cc789-c107-41b1-ada1-485a13aab70d",
           name: "PostgreSQL",
-          level: 1,
+          level: 2,
           subtopics: [],
           resources: [],
           examples: []
@@ -319,12 +319,12 @@ let roadmapBackEndData = {
         {
           id: "b71c92e1-a069-4d7a-a55c-0f65b41c3e2d",
           name: "Create Application With Learned Knowledge",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "e2601887-c529-4aeb-91ad-67c0ea6a41c3",
               name: "Implement Registration",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -332,7 +332,7 @@ let roadmapBackEndData = {
             {
               id: "8f8c7a81-fe2f-480c-9688-e8aaa138c21f",
               name: "Implement Login",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -340,7 +340,7 @@ let roadmapBackEndData = {
             {
               id: "badde9c5-b8a5-4021-a503-837b9fda13ed",
               name: "Implement CRUD",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -352,12 +352,12 @@ let roadmapBackEndData = {
         {
           id: "55a389dd-f9fb-4111-afca-4d955bc984c5",
           name: "Possible Project: Blog",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "a73d339d-ea65-4e7b-b5dd-4ef450150b50",
               name: "Registration Page",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -365,7 +365,7 @@ let roadmapBackEndData = {
             {
               id: "132a0c63-98ee-406c-9ac6-08580041e924",
               name: "Display Public Profile Page",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -373,7 +373,7 @@ let roadmapBackEndData = {
             {
               id: "edbfcb8d-a817-47ac-b266-7495f3c9f5b6",
               name: "Create, Update, Delete Posts",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -381,7 +381,7 @@ let roadmapBackEndData = {
             {
               id: "a1a6d8de-0f9a-45ba-b82a-e8c21c604969",
               name: "Display Posts On Profile Page",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -393,7 +393,7 @@ let roadmapBackEndData = {
         {
           id: "3ece5011-f54d-4290-9560-f6e3e9ce324d",
           name: "Write Tests",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -401,7 +401,7 @@ let roadmapBackEndData = {
         {
           id: "771955ae-ec5e-4d65-9da2-a95ec9873d4d",
           name: "Follow Standards And Best Practices",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -409,12 +409,12 @@ let roadmapBackEndData = {
         {
           id: "246cefd8-9e08-47b7-950c-600d6e21e5a8",
           name: "Incorporate Database",
-          level: 1,
+          level: 0,
           subtopics: [
             {
               id: "54093972-2518-4260-9502-04a60240f9d5",
               name: "Add Indexes",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -422,7 +422,7 @@ let roadmapBackEndData = {
             {
               id: "724b06c0-2f55-446e-aa9e-2c70402dc57d",
               name: "Use Proper Storage Engine",
-              level: 1,
+              level: 0,
               subtopics: [],
               resources: [],
               examples: []
@@ -430,12 +430,12 @@ let roadmapBackEndData = {
             {
               id: "94dadfa8-4429-41cb-a108-0c6224033ef5",
               name: "Protect From SQL Injection Attacks",
-              level: 1,
+              level: 0,
               subtopics: [
                 {
                   id: "adb7b2cc-73c1-428e-b95b-5324276b9e1f",
                   name: "Analyze Queries",
-                  level: 1,
+                  level: 0,
                   subtopics: [],
                   resources: [
                     "https://www.esecurityplanet.com/threats/how-to-prevent-sql-injection-attacks.html"
@@ -459,7 +459,7 @@ let roadmapBackEndData = {
         {
           id: "8951b907-0769-466f-9d81-783b16c289f9",
           name: "Express.js",
-          level: 1,
+          level: 3,
           subtopics: [],
           resources: [],
           examples: []
@@ -467,7 +467,7 @@ let roadmapBackEndData = {
         {
           id: "fc732684-2572-4a6c-bf28-ce259ff8bc0a",
           name: "Hapi.js",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
@@ -481,7 +481,7 @@ let roadmapBackEndData = {
         {
           id: "82c4f20b-519d-48b5-9f75-db963b231a6a",
           name: "Remake Previous App With A Framework",
-          level: 1,
+          level: 0,
           subtopics: [],
           resources: [],
           examples: []
