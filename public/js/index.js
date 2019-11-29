cfc517b5e947f46c098e8edcc369720a878e251b
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-29 14:36:34 -0500

    Changes title font size for better responsiveness.

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,15 @@ window.onscroll = function() {removeTitleHeader()};
 
 function removeTitleHeader() {
   let title = document.getElementsByClassName("navbar__nameContent__title")[0];
-  if (document.body.scrollTop > 58 || document.documentElement.scrollTop > 58) {
-    // title.style.display = "none";
-    title.style.fontSize = "0";
+  if (window.innerWidth <= 500) {
+    if (document.body.scrollTop > 58 || document.documentElement.scrollTop > 58) {
+      // title.style.display = "none";
+      title.style.fontSize = "0";
+    } else {
+      // title.style.display = "block";
+      title.style.fontSize = "1.1em";
+    }
   } else {
-    // title.style.display = "block";
-    title.style.fontSize = "1.2em";
+    title.style.fontSize = "1.1em";
   }
 }
