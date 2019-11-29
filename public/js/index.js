5172edc3597f85711517cf6378574ed3ddb3ffef
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-29 14:57:30 -0500

    Adds responsive font size change for name on 500px or less.

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,16 @@
 window.onscroll = function() {removeTitleHeader()};
 
 function removeTitleHeader() {
+  let name = document.getElementsByClassName("navbar__nameContent__name")[0];
   let title = document.getElementsByClassName("navbar__nameContent__title")[0];
   if (window.innerWidth <= 500) {
     if (document.body.scrollTop > 58 || document.documentElement.scrollTop > 58) {
       // title.style.display = "none";
+      name.style.fontSize = "2.5em";
       title.style.fontSize = "0";
     } else {
       // title.style.display = "block";
+      name.style.fontSize = "3em";
       title.style.fontSize = "1.1em";
     }
   }
